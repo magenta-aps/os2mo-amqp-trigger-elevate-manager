7e83d0d986d43382611570cc1eaf9d0969a0e450
fh@magenta-aps.dk <fh@magenta-aps.dk>
2023-02-22 10:45:49 +0100

    feat: [#54835] Configured gql client for mo

--- elevate_manager/config.py
+++ elevate_manager/config.py
@@ -1,21 +1,46 @@
 # SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+from typing import cast
 from fastramqpi.config import Settings as FastRAMQPISettings  # type: ignore
 from pydantic.env_settings import BaseSettings
 from pydantic.fields import Field
+from pydantic import AnyHttpUrl
+from pydantic import AmqpDsn
+from pydantic import parse_obj_as
 
 
 class Settings(BaseSettings):
-    fastramqpi: FastRAMQPISettings = Field(
-        default_factory=FastRAMQPISettings, description="FastRAMQPI settings"
-    )
+    """Settings for the engagement elevator AMQP trigger."""
 
     log_level: str = "INFO"
 
     class Config:
+        """Settings are frozen."""
+
         frozen = True
         env_nested_delimiter = "__"
 
+    amqp_url: AmqpDsn = Field(parse_obj_as(AmqpDsn, "amqp://guest:guest@msg_broker"))
 
-def get_settings(*args, **kwargs) -> Settings:
-    return Settings(*args, **kwargs)
+    auth_realm: str = Field("mo", description="Base URL for OS2mo. Will authenticate against this Realm.")
+
+    auth_server: AnyHttpUrl = Field(
+        parse_obj_as(AnyHttpUrl, "http://keycloak-service:8080/auth"),
+        description="Base URL for OIDC server, which is Keycloak."
+    )
+
+    client_id: str = Field("engagement-elevator", description="Client ID for Keycloak.")
+
+    client_secret: str | None = Field(..., description="Client secret for Keycloak.")
+
+    fastramqpi: FastRAMQPISettings = Field(
+        default_factory=FastRAMQPISettings, description="FastRAMQPI settings."
+    )
+
+    mo_url: AnyHttpUrl = Field(
+        parse_obj_as(AnyHttpUrl, "http://mo-service:5000"),
+        description="Base URL for OS2mo.",
+    )
+
+    mora_base: AnyHttpUrl = Field(cast(AnyHttpUrl, "http://localhost:5000"))
+
--- elevate_manager/mo.py
+++ elevate_manager/mo.py
@@ -4,7 +4,7 @@ from uuid import UUID
 import structlog
 from raclients.graph.client import PersistentGraphQLClient  # type: ignore
 
-from elevate_manager.config import Settings
+from config import Settings
 
 logger = structlog.get_logger()
 
@@ -13,6 +13,16 @@ def get_client(settings: Settings) -> PersistentGraphQLClient:
     """
     Configure and return GraphQL client
     """
+    gql_client = PersistentGraphQLClient(
+        url=f"{settings.mora_base}/graphql/v3",
+        client_id=settings.client_id,
+        client_secret=settings.client_secret,
+        auth_realm=settings.auth_realm,
+        auth_server=settings.auth_server,
+        sync=True,
+        httpx_client_kwargs={"timeout": None},
+    )
+    return gql_client
     logger.debug("Set up GraphQL client")
     pass
 
@@ -70,7 +80,7 @@ def get_org_unit_levels(gql_client: PersistentGraphQLClient):
 
 
 # TODO: add return type (Quicktype obj) for the appropriate GQL query
-def get_existing_managers(org_unit: UUID):
+def get_existing_managers(org_unit_uuid: UUID, gql_session: PersistentGraphQLClient):
     """
     Get existing managers of the given OU
 
@@ -95,7 +105,7 @@ def get_existing_managers(org_unit: UUID):
 
 # TODO: add argument providing existing manager(s) (can be None)
 def update_manager_and_elevate_engagement(
-    org_unit: UUID, elevate_engagement: bool
+        org_unit: UUID, elevate_engagement: bool
 ) -> None:
     """
     This function will:

544f4879d992c3c3599c9c7be9203fc80d37e2eb
Andreas Kring <andreas@magenta.dk>
2023-02-24 11:00:20 +0100

    Add more tests

--- tests/manual/test_ou_levels.py
+++ tests/manual/test_ou_levels.py
@@ -1,9 +1,11 @@
 # SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
+from parameterized import parameterized
 from more_itertools import one
 from pydantic import parse_obj_as
 
-from elevate_manager.models.get_org_unit_levels import GetOrgUnitLevels
+from elevate_manager.models.get_org_unit_levels import GetOrgUnitLevels, \
+    OrganisationUnit
 from elevate_manager.ou_levels import get_new_org_unit_for_engagement
 
 graphql_response = {
@@ -51,19 +53,44 @@ graphql_response = {
 }
 
 
-def test_get_new_org_unit_for_engagement():
+@parameterized.expand(
+    [
+        (
+            "NY6-niveau",
+            "NY5-niveau",
+            parse_obj_as(OrganisationUnit,
+                 {
+                     "name": "Borgmesterens Afdeling",
+                     "uuid": "b6c11152-0645-4712-a207-ba2c53b391ab",
+                     "org_unit_level": {
+                         "name": "NY6-niveau",
+                         "uuid": "84f95e29-48a0-4175-85fd-84a1f596e1a4"
+                     }
+                 }
+            )
+        ),
+        ("NY5-niveau", "NY6-niveau", None),
+        ("NY5-niveau", "NY5-niveau", None),
+    ]
+)
+def test_get_new_org_unit_for_engagement(
+    manager_ou_level_name, eng_ou_level_name, expected
+):
     # Arrange
     org_unit_levels = parse_obj_as(GetOrgUnitLevels, graphql_response)
     objects = one(one(org_unit_levels.data.managers).objects)
     manager_ou = one(objects.org_unit)
 
     # Set manager OU level
-    manager_ou.org_unit_level.name = "NY6-niveau"
+    manager_ou.org_unit_level.name = manager_ou_level_name
     # Set engagement ou level
-    one(one(one(objects.employee).engagements).org_unit).org_unit_level.name = "NY5-niveau"
+    one(one(
+        one(objects.employee).engagements).org_unit).org_unit_level.name = eng_ou_level_name
 
     # Act
     org_unit = get_new_org_unit_for_engagement(org_unit_levels)
 
     # Assert
-    assert org_unit == manager_ou
+    assert org_unit == expected
+
+# TODO: add non happy path tests

0d9e2e14b5260e0fabdeaa2d4c7cc54d5111edaa
Andreas Kring <andreas@magenta.dk>
2023-03-01 10:47:49 +0100

    Remove more redundant settings

--- elevate_manager/config.py
+++ elevate_manager/config.py
@@ -1,9 +1,6 @@
 # SPDX-FileCopyrightText: 2022 Magenta ApS <https://magenta.dk>
 # SPDX-License-Identifier: MPL-2.0
-from typing import cast
-
 from fastramqpi.config import Settings as FastRAMQPISettings  # type: ignore
-from pydantic import AmqpDsn
 from pydantic import Field
 from pydantic.env_settings import BaseSettings
 
@@ -18,8 +15,6 @@ class Settings(BaseSettings):
 
         frozen = True
         env_nested_delimiter = "__"
-
-    amqp_url: AmqpDsn = Field(cast(AmqpDsn, "amqp://guest:guest@msg_broker"))
 
     fastramqpi: FastRAMQPISettings = Field(
         default_factory=FastRAMQPISettings, description="FastRAMQPI settings."
